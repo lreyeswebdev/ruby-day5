6f10a4ebdb74a985d114184f00e0ec7f1b12e450
lreyeswebdev <68845154+lreyeswebdev@users.noreply.github.com>
2020-11-05 18:56:51 +0800

    revised example

--- square.rb
+++ square.rb
@@ -1,9 +1,18 @@
-def is_square(x)
-    (0..x).each do |ctr|
-        return true if (ctr*ctr) == x
-    end
-
+def perfect_square(x)
+    if x == 0
+        return true
+    else 
+        for y in 1..x
+            return true if (x.to_f / y.to_f) == y
+        end
+  
     false
+    end  
 end
-
-puts is_square(36) #true
+  
+puts perfect_square(-4) #false
+puts perfect_square(0) #true
+puts perfect_square(3) #false
+puts perfect_square(4) #true
+puts perfect_square(25) #true
+puts perfect_square(26) #false
